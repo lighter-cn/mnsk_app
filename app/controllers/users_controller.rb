ad695baf51d431ebf9d41443dd61bf6d95a438e4
lighter <zeke.kircheis@gmail.com>
2020-12-18 12:31:24 +0900

    create update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  before_action :get_user, only: [:show,:edit, :update]
+  def show
+    # カード情報取得
+    
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(update_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private 
+
+  def get_user
+    @user = User.find(current_user.id)
+  end
+
+  def update_params
+    params.require(:user).permit(:name, :email)
+  end
 end
