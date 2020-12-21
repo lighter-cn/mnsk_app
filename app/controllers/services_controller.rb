24263473535c66347213fc91f5c37ea7a4283099
lighter <zeke.kircheis@gmail.com>
2020-12-21 11:06:55 +0900

    create image post format

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -11,11 +11,24 @@ class ServicesController < ApplicationController
     @service = Service.new
   end
 
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def pull_user
     if user_signed_in?
       @user = User.find(current_user.id)
     end
+  end
+
+  def service_params
+    params.require(:service).permit(:service_name, :price, :explanation, :category_id, images: []).merge(service_status: "open", user_id: current_user.id)
   end
 end
