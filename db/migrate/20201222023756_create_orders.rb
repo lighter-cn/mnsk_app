fe7523d53c92c5f4bdb375861e8877aab715b0a6
lighter <zeke.kircheis@gmail.com>
2020-12-22 16:56:47 +0900

    modify orders table

--- db/migrate/20201222023756_create_orders.rb
+++ db/migrate/20201222023756_create_orders.rb
@@ -3,6 +3,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
     create_table :orders do |t|
       t.references :user    ,null: false, fireign_key: true
       t.references :service ,null: false, fireign_key: true
+      t.text :subscription  ,null: false
       t.timestamps
     end
   end
