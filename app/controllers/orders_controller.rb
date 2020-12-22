32288e8ecb9e6cd9c8e6acad5379807788752ef5
lighter <zeke.kircheis@gmail.com>
2020-12-22 12:14:02 +0900

    create view file

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :pull_user
+  def new
+    @service = Service.find(params[:service_id])
+    @order = Order.new
+  end
+  def create
+    @order = Order.new(params_order)
+  end
+
+  private
+
+  def pull_user
+    if user_signed_in?
+      @user = User.find(current_user.id)
+    end
+  end
+
+  def params_order
+  end
 end
