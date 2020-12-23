8409634c2b5eda2ba19df4d12d2f8667dfdf3b7c
lighter <zeke.kircheis@gmail.com>
2020-12-23 19:04:03 +0900

    create code model

--- db/migrate/20201222023756_create_orders.rb
+++ db/migrate/20201222023756_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.references :user    ,null: false, fireign_key: true
-      t.references :service ,null: false, fireign_key: true
+      t.references :user    ,null: false, foreign_key: true
+      t.references :service ,null: false, foreign_key: true
       t.text :subscription  ,null: false
       t.timestamps
     end
