02c370291059100c5b64c3904422ce77a81efbe3
lighter <zeke.kircheis@gmail.com>
2020-12-23 11:41:04 +0900

    refactoring html & css

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -9,7 +9,7 @@ class ServicesController < ApplicationController
 
   def show
     @service = Service.find(params[:id])
-    @order = Order.find_by user_id: current_user.id, service_id: @service.id
+    @order = Order.find_by(user_id: current_user.id, service_id: @service.id)
 
     unless @order.nil?
       # payjpの処理
