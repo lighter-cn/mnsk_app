faf4f3c0014df0906bf56d44528a21a45d091044
lighter <zeke.kircheis@gmail.com>
2020-12-25 14:47:59 +0900

    run rubocops

--- app/helpers/codes_helper.rb
+++ app/helpers/codes_helper.rb
@@ -1,8 +1,8 @@
 module CodesHelper
   require 'chunky_png'
 
-  def qrcode_tag(url, options = {})
+  def qrcode_tag(url, _options = {})
     qr = ::RQRCode::QRCode.new(url)
-    return ChunkyPNG::Image.from_datastream(qr.as_png.resize(250,250).to_datastream).to_data_url
+    ChunkyPNG::Image.from_datastream(qr.as_png.resize(250, 250).to_datastream).to_data_url
   end
 end
