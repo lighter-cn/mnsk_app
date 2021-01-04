eb91896c34c085d7ab24f4e747acaf9200721134
lighter <zeke.kircheis@gmail.com>
2021-01-04 14:25:00 +0900

    create card-model test code

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :card do
+    card_token {Faker::Internet.password(min_length: 20)}
+    customer_token {Faker::Internet.password(min_length: 20)}
+    association :user
   end
 end
