be6171a0bb6fa85f3ed7319081006d28e41c58ce
lighter <zeke.kircheis@gmail.com>
2021-01-04 14:54:48 +0900

    create service factory

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :service do
+    service_name   {Faker::String.random(length: 10..30)}
+    service_status {'open'}
+    price          {Faker::Number.between(from: 100, to: 100000)}
+    explanation    {Faker::String.random(length: 30..300)}
+    category_id    {Faker::Number.between(from: 2, to: 7)}
+
+    association :user
   end
 end
