4fb4f2ce2a25a906ca723268d3e83aeac6bf95d7
lighter <zeke.kircheis@gmail.com>
2021-01-12 22:06:23 +0900

    modify click action

--- app/javascript/good.js
+++ app/javascript/good.js
@@ -5,24 +5,27 @@ function good() {
   }
   good_btn.setAttribute("data-load","true");
   good_btn.addEventListener("click",()=>{
-    const orderId = good_btn.getAttribute("order-id");
-    const serviceId = good_btn.getAttribute("service-id");
-    const XHR = new XMLHttpRequest();
-    XHR.open("GET", `/services/${serviceId}/orders/${orderId}/good`,true);
-    XHR.responseType = "json";
-    XHR.send();
-    XHR.onload = () => {
-      if(XHR.status != 200){
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        return null;
-      }
-      const result = XHR.response.order;
-      if(result.good === true){
-        good_btn.setAttribute("data-check","true");
-      }else if (result.good === false) {
-        good_btn.removeAttribute("data-check");
-      }
-    };
+    const bad_btn = document.getElementById('bad');
+    if(bad_btn.getAttribute("data-check")!=="true"){
+      const orderId = good_btn.getAttribute("order-id");
+      const serviceId = good_btn.getAttribute("service-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/services/${serviceId}/orders/${orderId}/good`,true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        if(XHR.status != 200){
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;
+        }
+        const result = XHR.response.order;
+        if(result.good === true){
+          good_btn.setAttribute("data-check","true");
+        }else if (result.good === false) {
+          good_btn.removeAttribute("data-check");
+        }
+      };
+    }
   });
 }
 setInterval(good, 1000);
