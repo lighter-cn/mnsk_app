560730c73036f007e51e44074b2376e4d7941f8f
lighter <zeke.kircheis@gmail.com>
2021-01-13 19:31:58 +0900

    modify view & controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,36 @@ class UsersController < ApplicationController
   before_action :authenticate_user! # ログイン状態の確認
   before_action :pull_user, only: [:show, :edit, :update]
   def show
-    # カード情報取得
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    card = Card.find_by(user_id: current_user.id)
-
-    if card.present?
-      customer = Payjp::Customer.retrieve(card.customer_token)
-      @card = customer.cards.first
-    else
-      @card = nil
+    # 本人確認
+    if current_user.id == @user.id
+      # カード情報取得
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+      card = Card.find_by(user_id: @user.id)
+  
+      if card.present?
+        customer = Payjp::Customer.retrieve(card.customer_token)
+        @card = customer.cards.first
+      else
+        @card = nil
+      end
+  
+      # サービス取得
+      @services = Service.getServiceUserCount(@user.id)
+  
+      # オーダー取得
+      @orders = Order.getBuyOrders(@user.id)
+    else 
+      @services = Service.getServiceUserCount(@user.id)
     end
-
-    # サービス取得
-    @services = Service.getServiceUserCount(@user.id)
-
-    # オーダー取得
-    @orders = Order.getBuyOrders(@user.id)
   end
 
   def edit
+    redirect_to user_path unless current_user.id == @user.id
   end
 
   def update
+    redirect_to user_path unless current_user.id == @user.id
+    
     if @user.update(update_params)
       redirect_to user_path
     else
@@ -35,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def pull_user
-    @user = User.find(current_user.id) if user_signed_in?
+    @user = User.find(params[:id]) if user_signed_in?
   end
 
   def update_params
